e85a8446b966c0399eca8f498f045beb308ecc4e
Larissa T <larissa.ket@hotmail.com>
2021-02-26 21:41:30 -0300

    Adicionar arquivos de projeto.

---
+++ Proj_Jr_Web_Dev/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Proj_Jr_Web_Dev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
